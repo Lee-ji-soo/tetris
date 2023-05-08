3c512ceae6a7e49da508ea619bf31ebd22972b62
Lee-ji-soo <nbsp3@naver.com>
2023-05-09 01:58:22 +0900

    create block

--- project/main.cpp
+++ project/main.cpp
@@ -1,13 +1,81 @@
 #include <iostream>
 #include "block.h"
+#include <windows.h>
 using namespace std;
 
+int dy = 0;
+int dx = 3;
+
+int map[10][10] = {
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+void print()
+{
+    system("cls");
+
+    int buf[10][10];
+    memcpy(buf, map, 10 * 10 * 4);
+
+    // block to buf
+    for (int y = 0; y < 4; y++)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            if (block[y][x] == 1)
+            {
+                buf[dy + y][dx + x] = 1;
+            }
+        }
+    }
+
+    for (int y = 0; y < 10; y++)
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            if (buf[y][x] == 0)
+            {
+                cout << ' ';
+            }
+            else
+            {
+                cout << '#';
+            }
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     setBlock();
-    spin();
-    pull();
-    spin();
-    pull();
+    while (1)
+    {
+        print();
+        if (GetAsyncKeyState(VK_UP) & 0x8001)
+        {
+            spin();
+        }
+        if (GetAsyncKeyState(VK_DOWN) & 0x8001)
+        {
+            dy++;
+        }
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8001)
+        {
+            dx++;
+        }
+        if (GetAsyncKeyState(VK_LEFT) & 0x8001)
+        {
+            dx--;
+        }
+        Sleep(100);
+    }
     return 0;
 };
